5292cf3088c4a093853766a0fbe73ae961b2bea1
Derya Akan <d.akan369@gmail.com>
2024-03-20 01:58:13 +0300

    All problems are solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,30 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        return isBarking && (clock < 8 || clock > 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19) || (secondAge >= 13 && secondAge <= 19) || (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        return !isSummer || temp <= 45;
     }
 
     public static double area(double width, double height) {
-       
+        if(width < 0 || height < 0){
+            return -1;
+        }
+        else return width * height;
     }
 
     public static double area(double radius) {
-       
+        double pi = Math.PI;
+        if(radius < 0){
+            return -1;
+        }
+
+       return pi * radius * radius;
     }
 }
